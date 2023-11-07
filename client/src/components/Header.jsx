95e7c92923744c6f6c9ff81aa867ccedb9b3b1e4
Rakesh Saraswat <rgsaraswat2002@gmail.com>
2023-11-08 05:05:55 +0530

    updating header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,19 @@
-import {FaSearch} from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-slate-200 shadow border-slate-400">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to='/'>
         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">RoseWill</span>
-          <span className="text-slate-700">Estates</span>
+          <span className="text-slate-500">Project</span>
+          <span className="text-slate-700 uppercase">One</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-slate-50 p-3 rounded-lg flex items-center'>
           <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
           <FaSearch className='text-slate-600'/>
         </form>
@@ -22,9 +24,17 @@ export default function Header() {
             <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
-            <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'>SignIn</li>
-            </Link>
+            <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
         </ul>
       </div>
     </header>
